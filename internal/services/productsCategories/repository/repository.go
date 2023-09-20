6b568b66f0c94bdcc0cfd2308087d3567208de7e
Андрей Кравцов <kan19u508@student.bmstu.ru>
2023-09-20 16:59:50 +0300

    .

--- internal/services/productsCategories/repository/repository.go
+++ internal/services/productsCategories/repository/repository.go
@@ -182,7 +182,15 @@ func (a productsCategoriesRepository) AddProduct(ctx context.Context, Product *m
 	// timeout 15 sek
 	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
 	defer cancel()
-	log.Println(ctx, query.Request)
+	log.Println("_______________________________")
+	err = a.conn.Ping(ctx)
+	if err != nil {
+		log.Println("PING ERR err")
+	} else {
+		log.Println("PING OK")
+	}
+	log.Println(ctx, query.Request, query.Params)
+	log.Println("_______________________________")
 	row := a.conn.QueryRow(ctx, query.Request, query.Params...)
 	log.Println("a.conn.QueryRow - OK")
 	if err := row.Scan(
