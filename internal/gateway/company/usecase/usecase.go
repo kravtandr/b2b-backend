4e844c8ea613ff90311043d10ab2cf235017cdbb
Андрей Кравцов <kan19u508@student.bmstu.ru>
2024-03-17 17:15:50 +0300

    fix id #2

--- internal/gateway/company/usecase/usecase.go
+++ internal/gateway/company/usecase/usecase.go
@@ -117,6 +117,7 @@ func (u *companyUseCase) GetCompanyByProductId(ctx context.Context, id int64) (*
 	}
 
 	return &models.Company{
+		Id:           responce.Id,
 		Name:         responce.Name,
 		Description:  responce.Description,
 		LegalName:    responce.LegalName,
