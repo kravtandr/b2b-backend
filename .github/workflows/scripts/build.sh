1cc8c3fd85530756f778dec37cbf94a2ac9aefa5
Your Name <you@example.com>
2024-08-10 20:29:57 +0300

    remove --env-file from docker in ci

--- .github/workflows/scripts/build.sh
+++ .github/workflows/scripts/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Build..."
 cd /var/lib/postgresql/backend/b2b-backend && git pull
-docker-compose build --parallel --no-cache --env-file=.env
+docker-compose build --parallel --no-cache
 echo "Build completed"
