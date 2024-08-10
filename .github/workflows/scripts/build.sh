d3fe4bd70be413c976f3b170c8ec4039c9682855
Your Name <you@example.com>
2024-08-10 20:25:37 +0300

    add .env

--- .github/workflows/scripts/build.sh
+++ .github/workflows/scripts/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Build..."
 cd /var/lib/postgresql/backend/b2b-backend && git pull
-docker-compose build --parallel --no-cache
+docker-compose build --parallel --no-cache --env-file=.env
 echo "Build completed"
--- .github/workflows/scripts/start.sh
+++ .github/workflows/scripts/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Start docker-compose"
 cd /var/lib/postgresql/backend/b2b-backend
-docker-compose up -d 
+docker-compose up -d --remove-orphans
